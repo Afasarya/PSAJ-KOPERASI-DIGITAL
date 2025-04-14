963f19528e48701883436df0c0efac65cdb64706
Afasarya <coderea9@gmail.com>
2025-04-14 21:26:48 +0700

    update:npm-run-build

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
@@ -74,7 +74,7 @@ export default function UpdateProfileInformation({
                     <InputError className="mt-2" message={errors.email} />
                 </div>
 
-                {mustVerifyEmail && user.email_verified_at === null && (
+                {mustVerifyEmail && (user as any).email_verified_at === null && (
                     <div>
                         <p className="mt-2 text-sm text-gray-800 dark:text-gray-200">
                             Your email address is unverified.
